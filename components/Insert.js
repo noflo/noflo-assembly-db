9aa1dee4825832801774b262322c236afc5ca448
Vladimir Sibirov <trustmaster@kodigy.com>
2017-05-10 13:10:10 +0300

    Add safeguard against double sends
    which happens because .catch() will catch all exceptions in the app regardless

--- components/Insert.js
+++ components/Insert.js
@@ -15,15 +15,22 @@ class Insert extends Component {
   relay(msg, output) {
     const returning = msg.query.returning != null ? msg.query.returning : undefined;
 
+    let sent = false;
     msg.db(msg.query.table)
     .insert(msg.query.data, returning)
     .then((rows) => {
       msg.rowset = rows;
       msg.query = undefined;
-      output.sendDone(msg);
+      if (!sent) {
+        output.sendDone(msg);
+        sent = true;
+      }
     }).catch((err) => {
-      err.message += ' [in assembly-db/Insert]';
-      output.sendDone(fail(msg, err));
+      if (!sent) {
+        err.message += ' [in assembly-db/Insert]';
+        output.sendDone(fail(msg, err));
+        sent = true;
+      }
     });
   }
 }
